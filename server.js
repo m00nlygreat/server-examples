eef1ad267d9ed3685f867ba1423d187f2e60d084
배문형 <m00nlygreat@baemunhyeongs-MacBook-Air.local>
2024-07-04 20:57:15 +0900

    public 폴더 사용

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname, '/public'));
+
 app.listen(8080, () => {
     console.log('listening on port 8080');
     })
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.sendFile(__dirname + '/index.html');
     })
